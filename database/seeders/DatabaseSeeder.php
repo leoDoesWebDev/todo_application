15a44e00ff413db3ec9d817af3d27ac2b4ac95f7
lleo <leo.edwards@in2tel.ie>
2021-05-09 16:39:40 +0100

    Todo list migration added and some sample data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace Database\Seeders;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
 
 use Illuminate\Database\Seeder;
 
@@ -13,6 +15,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('todo')->insert([
+            'task' => 'Pop to the shops',
+            'description' => 'Buy eggs, milk, butter, flour, peppers, ham, cheese and bread',
+            'complete_by' => '2021-05-20',
+            'status' => 0,
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('todo')->insert([
+            'task' => 'Complete todo application',
+            'description' => 'Create a todo application that demonstrates MVC knowledge',
+            'complete_by' => '2021-05-10',
+            'status' => 1,
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
     }
 }
