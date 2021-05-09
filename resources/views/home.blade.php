e5acee3180e51d93b4cfd74586363f71868794e9
lleo <leo.edwards@in2tel.ie>
2021-05-09 16:19:02 +0100

    Datatable package installed & works with todo table

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,19 @@
         <div class="card">
             <div class="card-header">Todo List</div>
             <div class="card-body">
-
+                <table id="todo_table" class="table">
+                    <thead>
+                    <tr>
+                        <th scope="col">Task</th>
+                        <th scope="col">Description</th>
+                        <th scope="col" >Complete By</th>
+                        <th scope="col">Status</th>
+                        <th scope="col">Actions</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
@@ -16,6 +28,10 @@
 
         $(function (e){
             console.log('jquery  working');
+
+            let todoTable = $('#todo_table').DataTable({});
+
+
         })
     </script>
 @endsection
