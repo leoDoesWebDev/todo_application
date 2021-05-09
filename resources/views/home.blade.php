d60b6b7213c84569bcf87f7473b6856f7310e6e7
lleo <leo.edwards@in2tel.ie>
2021-05-09 16:24:49 +0100

    Table has sample data in memory

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -29,9 +29,33 @@
         $(function (e){
             console.log('jquery  working');
 
-            let todoTable = $('#todo_table').DataTable({});
+            let dataSet = [
+                {
+                    'task': 'Wash clothes',
+                    'description': 'Wash mixed load at 30 degrees for 1 hour',
+                    'complete_by': '20/08/2021',
+                    'status': '0',
+                    'id': '1',
+                },
+                {
+                    'task': 'Hair Cut',
+                    'description': 'At Jimmy\'s barbers',
+                    'complete_by': '21/08/2021',
+                    'status': '0',
+                    'id': '2',
+                },
+            ];
 
-
+            let todoTable = $('#todo_table').DataTable({
+                data: dataSet,
+                "columns": [
+                    {"data": "task"},
+                    {"data": "description"},
+                    {"data": "complete_by"},
+                    {"data": "status"},
+                    {"data": "id"},
+                ]
+            });
         })
     </script>
 @endsection
